b14977f863a4d457f07e07e4c0ecfea1d245ae09
Evan Zhou <evanzhou@EvandeMacBook-Pro.local>
2024-11-26 16:48:22 +0800

    feat: 去掉CI的单元测试部分

--- test.mjs
+++ test.mjs
@@ -1,7 +1,5 @@
 import * as bleHeartRate from './index.js';
 
-// const bleHeartRate = new BleHeartRate();
-
 bleHeartRate.start((device_id, heart_rate) => {
   console.log('result', device_id, heart_rate);
 });
